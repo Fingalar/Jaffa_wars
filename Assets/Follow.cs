1bfdd529ec49351228397e515173aab4a67bedd2
Fingalar <fingalar@localhost.localdomain>
2014-08-29 23:11:31 +0200

    Camera utilisable  mais a revoir

---
+++ Assets/Follow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform ship;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = new Vector3(ship.eulerAngles.x, ship.eulerAngles.y, ship.eulerAngles.z);
+	}
+}
---
+++ Assets/FollowShip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowShip : MonoBehaviour {
+
+	public Transform ship;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
